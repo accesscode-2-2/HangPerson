c3ed5d604c646a6c4c700110f0a4a6dfcb6541c0
Daniel Distant <daniel.distant1@gmail.com>
2015-06-16 19:07:23 -0400

    First commit, just ideas so far

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -8,10 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+int guesses word {
+    
+    int guesses;
+    
+}
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // code goes here...
+        //word is 'taekwondo'
+        
+        printf("Guess the word!\n\n_ _ _ _ _ _ _ _ _\n\nYou have 12 guesses left!");
+        
+        /* Probably need some char action
+         
+         blank spaces _ _ _ _ _ _ _ _ _ <--like this
+         
+         every time for user input I need a scanf
+         
+         loop through every character and compare it to guess 
+         
+         if guess matches a letter, do the 'swap' function with it
+         
+         build in a hint
+         
+         
+         */
+         
+        printf("Hello world!");
 
     }
     return 0;

5b6c4ebec5c510c8cbd1e43876f50b44093ef623
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-06-19 16:03:17 -0400

    homework for pull request

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -11,7 +11,48 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // code goes here...
+        char myWord[6] = "agave";
+        char myDashes[6] = "_____";
+        char guess;
+        int count = 0;
+        int found;
+        int ret;
+        int hasWon = 0;
+        
+        
+        printf("I'm thinking of a word. Can you guess it? \n");
+        while (count < 6 && !hasWon) {
+            found = 0;
+            printf("\n Please guess a letter. \n");
+            scanf("%c", &guess);
+            for (int j = 0; j < sizeof(myWord)/sizeof(char); j++) {
+                if (guess == myWord[j]) {
+                    myDashes[j] = myWord[j];
+                    found = 1;
+                }
+            }
+            for (int i = 0; i < sizeof(myWord)/sizeof(char); i++) {
+                printf("%c", myDashes[i]);
+            }
+            
+            scanf("%c", &guess);
+            
+            ret = strcmp(myDashes, myWord);
+            if (ret == 0) {
+                printf("You win!");
+                hasWon = 1;
+            }
+            
+            
+            if (found == 0) {
+                count = count + 1;
+            }
+            if (count == 6) {
+                printf("You lost!");
+            }
+            
+        }
+        
 
     }
     return 0;

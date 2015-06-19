1ef079abce904efc20860a3d9c69beeb64865332
Justine Gartner <GartnerJustine@gmail.com>
2015-06-18 23:22:59 -0400

    remove bug

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -20,7 +20,7 @@ int main(int argc, const char * argv[]) {
         //I would also like to add a maximum number of guesses
         
         
-        char guessedLette;
+        char guessedLetter;
         char secretWord1[7] = "unicorn";
         char blankSpaces1[7] = "_______";
         

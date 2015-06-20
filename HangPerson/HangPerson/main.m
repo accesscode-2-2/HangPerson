030b52ae662fbdd0d175b79cf4b21d3c936521b9
elberdev <elberdev@gmail.com>
2015-06-20 10:18:45 -0400

    lots of changes/refactoring

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -24,7 +24,7 @@ void titleScreen() {
     usleep(1e6);
 }
 
-// This function sets up the difficulty
+// This function sets up the game difficulty
 int difficultySetup(int maxWrongGuesses) {
     while (maxWrongGuesses == 0) {
         int difficulty;
@@ -34,13 +34,13 @@ int difficultySetup(int maxWrongGuesses) {
         
         switch (difficulty) {
             case 1:
-                maxWrongGuesses = 10;
+                maxWrongGuesses = 12;
                 break;
             case 2:
-                maxWrongGuesses = 8;
+                maxWrongGuesses = 10;
                 break;
             case 3:
-                maxWrongGuesses = 6;
+                maxWrongGuesses = 8;
                 break;
             default:
                 printf("\n\n  Sorry, you did not input a number from 1 to 3.\n");
@@ -48,13 +48,127 @@ int difficultySetup(int maxWrongGuesses) {
         }
     }
     
+    printf("\n  Okay, let's begin!\n\n");
+    usleep(1e6);
+    printf("  You have a maximum of %d wrong guesses\n", maxWrongGuesses);
+    printf("  before you lose.\n\n");
+    usleep(1e6);
+    
     return maxWrongGuesses;
+}
+
+// This function picks a random word from the wordPool
+char * wordSetup(size_t wordPoolSize, char *wordPool[]) {
+
+    printf("  What word am I thinking of?\n");
+    
+    // pick a random index from the wordpool
+    srand((int) time(NULL));
+    int randomWordPoolIndex = rand()%wordPoolSize;
+    
+    // temp variable to hold word we pluck out of the word pool
+    char *gameWord = wordPool[randomWordPoolIndex];
+    return gameWord;
+}
+
+// This function builds the game board
+char * getGameBoard(char * gameWord, size_t gameWordSize) {
+    // board will be the size of temp with extra spaces between each
+    // character
+    int boardSize = ((int) gameWordSize * 2 - 1);
+    char board[boardSize];
+    
+    // fill board string with alternating uderscores and spaces
+    for (int i = 0; i < gameWordSize; i++) {
+        // all indexes that are multiples of 2 will hold underscores
+        board[i * 2] = '_';
+        // everything else in between will hold spaces. the if statement
+        // ensures there is no space following the last underscore
+        if (i < gameWordSize - 1) {
+            board[i * 2 + 1] = ' ';
+        } else {
+            board[i * 2 + 1] = '\0';
+        }
+    }
+    
+    char * returnBoard = board;
+    return returnBoard;
+}
+
+//void gameOn(int correctLetters, size_t gameWordSize, int wrongGuesses,
+//            int maxWrongGuesses, char * board, char letterGuess,
+//            char * gameWord) {
+//    // game logic. keeps running as long as you have not used up all your
+//    // guesses and have not guessed all the letters of the word
+//    while (correctLetters < gameWordSize && wrongGuesses < maxWrongGuesses) {
+//        
+//        // print inital board
+//        printf("\n\n      %s", board);
+//        
+//        printf("\n\n\n  Guess a letter: ");
+//        scanf("%c%*c", &letterGuess);
+//        
+//        // set up a boolean switch tell the program what to print in
+//        // case of a correct or incorrect guess
+//        BOOL correctGuess = 0;
+//        
+//        // check if the letter is in the word
+//        for (int i = 0; i < gameWordSize; i++) {
+//            if (letterGuess == gameWord[i]) {
+//                
+//                // if it is, update the display
+//                board[i*2] = letterGuess;
+//                // update conditionals for the while loop
+//                correctLetters++;
+//                correctGuess = 1;
+//                
+//            }
+//        }
+//        
+//        // print different things depending on whether the guess was
+//        // correct or not. remind the player how many guesses they
+//        // already used.
+//        if (correctGuess == 0) {
+//            printf("\n\n  Sorry, that's not in the word.\n");
+//            wrongGuesses++;
+//            if (wrongGuesses == 1) {
+//                printf("  That's %d wrong guess!", wrongGuesses);
+//            } else {
+//                printf("  That's %d wrong guesses!", wrongGuesses);
+//            }
+//            
+//        }
+//    }
+//}
+
+// This function prints the end of game results
+void endGame(int correctLetters, size_t gameWordSize, char * gameWord) {
+    
+    // print different ending message depending on result
+    if (correctLetters == gameWordSize) {
+        printf("\n  The word was: \n");
+        usleep(1e6);
+        printf("\n      <<<<< %s >>>>>\n", gameWord);
+        usleep(1e6);
+        printf("\n\n  YOU GOT IT!\n\n");
+        printf("  HURRAY FOR YOU.\n");
+        printf("  A COOL IMAGINARY VICTORY SCREEN APPEARS.\n");
+    } else {
+        printf("  YOU ARE OUT OF GUESSES!\n");
+        printf("\n  The word was: \n");
+        usleep(1e6);
+        printf("\n      <<<<< %s >>>>>\n", gameWord);
+        usleep(1e6);
+        printf("\n\n    YOU LOST!\n");
+        printf("    YOU KILLED AN IMAGINARY STICK FIGURE.\n");
+        printf("    GOOD GOING.\n\n");
+    }
 }
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
 
-//***************** OUR VARIABLES ************************************************//
+        //********* OUR VARIABLES ************************************************//
         
         char *wordPool[] = {"decapitated", "slum", "slush", "fuzzy", "sloppy",
             "slept", "quarry", "nozzle", "shank", "quartz", "iterate", "hangman",
@@ -64,12 +178,19 @@ int main(int argc, const char * argv[]) {
             "technologies", "catacombs", "inserted", "animal", "carpet",
             "lazybones", "molasses", "carnival", "compass", "jelly",
             "compact", "renaissance", "fantastic", "killer", "somber",
-            "animal", "arrow", "sloppy", "super", "superfluous", NULL};
+            "animal", "arrow", "sloppy", "super", "superfluous", "frugal",
+            "innovator", "serpent", "wisdom", "technique", "breakthrough",
+            "meditation", "thought", "durable", "succeeded",
+            "supercalifragilisticexpialadocious", NULL};
         
         // get the length of the wordPool array
         size_t wordPoolSize = arraySize(wordPool);
         // will hold the player's guesses
         char letterGuess;
+        // will hold the random word we pick and the display board
+        char *gameWord, *board;
+        // this value will be used very often for loops
+        size_t gameWordSize;
         // will keep track of how many wrong guesses
         int wrongGuesses = 0;
         // will be used to set difficulty
@@ -78,46 +199,27 @@ int main(int argc, const char * argv[]) {
         // know when the player wins
         int correctLetters = 0;
         
-//********************************************************************************//
         
+        //********* OUR GAME BEGINS **********************************************//
+        
+        // print title screen
         titleScreen();
+        
+        // setup the difficulty
         maxWrongGuesses = difficultySetup(maxWrongGuesses);
         
-        // initial setup
-        printf("\n  Okay, let's begin!\n\n");
-        usleep(1e6);
-        printf("  You have a maximum of %d wrong guesses\n", maxWrongGuesses);
-        printf("  before you lose.\n\n");
-        usleep(1e6);
-        printf("  What word am I thinking of?\n");
+        // pick a random word from the wordPool
+        gameWord = wordSetup(wordPoolSize, wordPool);
         
-        // pick a random index from the wordpool
-        srand((int) time(NULL));
-        int randomWordPoolIndex = rand()%wordPoolSize;
+        // calculate size of gameWord to use with loops
+        gameWordSize = (int) strlen(gameWord);
         
-        // temp variable to hold word we pluck out of the word pool
-        char *temp = wordPool[randomWordPoolIndex];
-        
-        // setup a separate string to hold the display of the word in the game
-        // this is the size it needs to be in order to have spaces between
-        // each letter
-        char board[(strlen(temp) * 2) - 1];
-        
-        // fill board string with alternating uderscores and spaces
-        for (int i = 0; i < strlen(temp); i++) {
-            // all indexes that are multiples of 2 will hold underscores
-            board[i * 2] = '_';
-            // everything else in between will hold spaces. the if statement
-            // ensures there is no space following the last underscore
-            if (i < strlen(temp) - 1) {
-                board[i * 2 + 1] = ' ';
-            }
-            
-        }
+        // create the game display board
+        board = getGameBoard(gameWord, gameWordSize);
         
         // game logic. keeps running as long as you have not used up all your
-        // guesses and have not guessed all the letters
-        while (correctLetters < strlen(temp) && wrongGuesses < maxWrongGuesses) {
+        // guesses and have not guessed all the letters of the word
+        while (correctLetters < gameWordSize && wrongGuesses < maxWrongGuesses) {
             
             // print inital board
             printf("\n\n      %s", board);
@@ -130,8 +232,8 @@ int main(int argc, const char * argv[]) {
             BOOL correctGuess = 0;
             
             // check if the letter is in the word
-            for (int i = 0; i < strlen(temp); i++) {
-                if (letterGuess == temp[i]) {
+            for (int i = 0; i < gameWordSize; i++) {
+                if (letterGuess == gameWord[i]) {
                     
                     // if it is, update the display
                     board[i*2] = letterGuess;
@@ -157,25 +259,9 @@ int main(int argc, const char * argv[]) {
             }
         }
         
-        // print the board one last time before the end
-        printf("\n\n      %s\n\n\n", board);
-        
-        // print different ending message depending on result
-        if (correctLetters == strlen(temp)) {
-            printf("  YOU GOT IT!\n\n");
-            printf("  HURRAY FOR YOU.\n");
-            printf("  A COOL IMAGINARY VICTORY SCREEN APPEARS.\n");
-        } else {
-            printf("  YOU ARE OUT OF GUESSES!\n");
-            printf("\n  The word was: \n");
-            usleep(1e6);
-            printf("  <<<<< %s >>>>>\n", temp);
-            printf("\n\n    YOU LOST!\n");
-            printf("    YOU KILLED AN IMAGINARY STICK FIGURE.\n");
-            printf("    GOOD GOING.\n\n");
-        }
+        // print game results
+        endGame(correctLetters, gameWordSize, gameWord);
 
-        
     }
     return 0;
 }

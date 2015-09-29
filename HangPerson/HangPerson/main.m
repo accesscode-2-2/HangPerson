44aa2913eb629576e022352519234eba2dfc41ce
Umar <umahmud@outlook.com>
2015-09-29 02:17:08 -0400

    done

--- HangPerson/HangPerson/main.m
+++ HangPerson/HangPerson/main.m
@@ -6,31 +6,45 @@
 //  Copyright (c) 2015 Mike Kavouras. All rights reserved.
 //
 
-#include <stdio.h>
+#import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    
-    char word[] = "code";
-    char guess;
-    
-    printf("Enter a letter: ");
-    
-    for (int i = 0; i < 10; i++) {
+    @autoreleasepool {
         
-        scanf("%c", &guess);
+        char word[9] = "computer";
+        char dash[9] = "________";
+        char guess;
+        int counter = 0;
+        int comparison = 0;
+        int match;
+        int result = 0;
         
-        if (guess == word [i]) {
-            guess = word [i];
-            printf("%c", word[i]);
+        while (counter < 8 && !result) {
+            match = 0;
+            printf("\nGuess a letter: ");
+            scanf("%c", &guess);
+            for (int i = 0; i < strlen(word); i++) {
+                if (guess == word[i]) {
+                    dash[i] = word[i];
+                    match = 1;
+                }
+            }
+            for (int j = 0; j < strlen(word); j++) {
+                printf("%c", dash[j]);
+            }
+            scanf("%c", &guess);
+            comparison = strcmp(dash, word);
+            if (match == 0) {
+                counter++;
+            }
+            if (comparison == 0) {
+                result = 1;
+                printf("\nYou win!");
+            }
+            if (counter == 8) {
+                printf("\nYou lose!");
+            }
         }
-        
-        if (i == 9) {
-            
-            printf("You lose!");
-            
-        }
-        
     }
-    
     return 0;
 }
